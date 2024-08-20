ba58088e55b22de058b012b6e1670bce867ea7a9
Melanie Akau <mkkakau@gmail.com>
2024-08-19 19:47:36 -1000

    Configure @catppuccin/tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@catppuccin/tailwindcss")],
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "frappe",
+    }),
+  ],
 };
 export default config;
